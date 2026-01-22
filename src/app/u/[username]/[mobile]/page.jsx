e765d325646681a384f9feebb19843e0e9d57eeb
Neelesh Meena <66689602+DamriaNeelesh@users.noreply.github.com>
2026-01-22 14:26:19 +0530

    fix(profile): correct UserProfileService import path

--- src/app/u/[username]/[mobile]/page.jsx
+++ src/app/u/[username]/[mobile]/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { UserProfileService } from "../../../../../services/userProfileService";
+import { UserProfileService } from "../../../../services/userProfileService";
 import {
     Box,
     Container,
